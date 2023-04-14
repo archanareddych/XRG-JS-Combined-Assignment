f6e0340d9543b8c8c4db22bf399e39a23988f56d
archanareddych <118352177+archanareddych@users.noreply.github.com>
2023-04-14 16:22:07 +0530

    initial commit to update

--- indexTwo.js
+++ indexTwo.js
@@ -4,7 +4,31 @@
 // Create Notes
 
 // Read Notes
+const readNotes = (id) => {
+  input.map((note) => {
+    if (note.id == id) {
+      return note;
+    }
+  });
+};
 
 // Update Notes
+const notes = [
+  { id: 1, title: 'great monk', author: 'jayshety' },
+  { id: 1, title: 'great father', author: 'william' },
+  { id: 1, title: 'godfather', author: 'asdfgh' },
+  { id: 1, title: 'marvel', author: 'defghh' },
+];
+
+const updateNote = (notes) => {
+  for (const udtnote in notes) {
+    if (udtnote.id == notes.id) {
+      udtnote.title = notes.title;
+      udtnote.author = notes.author;
+    }
+    return udtnote;
+  }
+};
+console.log(updateNote('great monk'));
 
 // Delete Notes
